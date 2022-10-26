d49f124c85662c3b2d5c77df68fbdcccfa0fa98c
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2022-10-26 15:33:25 -0400

    formatted pages and fixed path error in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ const secondPageEl = document.querySelector('#second-page');
 
 
 //refreshes the results of autocomplete with every key action
-$('#search').on('keyup',function(){
+$('#search').on('keyup', function () {
   let sVal = $(this).val();
 
-  if(sVal.length > 2){
-  autoComplete(sVal);
+  if (sVal.length > 2) {
+    autoComplete(sVal);
   }
 });
 
@@ -27,8 +27,8 @@ function autoComplete() {
       let dResult = [];
       $('#search-result').html(''); //clears the search result with every key action
       for (let i = 0; i < data.features.length; i++) {
-        dResult.push(data.features[i].properties.address_line1 +', '+ data.features[i].properties.address_line2)
-        $('#search-result').append('<option value='+dResult[i]+'>'+dResult[i]+'</option>');
+        dResult.push(data.features[i].properties.address_line1 + ', ' + data.features[i].properties.address_line2)
+        $('#search-result').append('<option value=' + dResult[i] + '>' + dResult[i] + '</option>');
       }
       $('#search-result').addClass('active');
       console.log(data);
@@ -40,48 +40,48 @@ function autoComplete() {
 }
 
 //chooses the element from the dropdown and clears the dropdown after selected
-$(document).on('click','#search-result option',function(){
-  $('#search').val($(this).text()); 
+$(document).on('click', '#search-result option', function () {
+  $('#search').val($(this).text());
   $('#search-result').html();
   $('#search-result').removeClass('active');
 })
 
 //searches the location and gets Yelp data
 function searchLocation() {
-let yelpAPI = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${searchEl.value}`;
+  let yelpAPI = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${searchEl.value}`;
 
-fetch(yelpAPI, {
-headers: {Authorization: 'Bearer 3FBTNebxKSbfJxZAZHGeldEJJ4C2qbP1hcVru2evp3VpDMBukDxQQkShPia8JuYiJuu8aQk1bCuXBGPhscsgO0XjBW4jjMHO_x2lV3TiIqzxnTFxe1H1KGmrqx1XY3Yx'}
-})
-  .then(function (response) { //then turns into JavaScript object
-    return response.json();
-})
-  .then(function (data) { //then lists the data to be used
-  console.log(searchEl.value)
-  console.log(data);
+  fetch(yelpAPI, {
+    headers: { Authorization: 'Bearer 3FBTNebxKSbfJxZAZHGeldEJJ4C2qbP1hcVru2evp3VpDMBukDxQQkShPia8JuYiJuu8aQk1bCuXBGPhscsgO0XjBW4jjMHO_x2lV3TiIqzxnTFxe1H1KGmrqx1XY3Yx' }
+  })
+    .then(function (response) { //then turns into JavaScript object
+      return response.json();
+    })
+    .then(function (data) { //then lists the data to be used
+      console.log(searchEl.value)
+      console.log(data);
 
-  for (let i = 0; i < data.businesses.length; i++) {
-    let listOfBusinesses = document.createElement('ol');
-    secondPageEl.appendChild(listOfBusinesses);
-    listOfBusinesses.textContent = data.businesses[i].name; //gets the name of the businesses
-    // listOfBusinesses.textContent = data.businesses[i].location.display_address;
-  }
+      for (let i = 0; i < data.businesses.length; i++) {
+        let listOfBusinesses = document.createElement('ol');
+        secondPageEl.appendChild(listOfBusinesses);
+        listOfBusinesses.textContent = data.businesses[i].name; //gets the name of the businesses
+        // listOfBusinesses.textContent = data.businesses[i].location.display_address;
+      }
 
-}).catch(error => console.log('error', error));
+    }).catch(error => console.log('error', error));
 
-// window.location.replace("nextPage.html");
-mainContainerEl.classList.add('hide');
+  // window.location.replace("nextPage.html");
+  mainContainerEl.classList.add('hide');
 }
 
 let dateRangePickerEl = document.querySelector(`[value="${moment().format('L')}"]`);
 
-$(function() {
-    $('input[name="daterange"]').daterangepicker({
-      opens: 'left'
-    }, function(start, end, label) {
-      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
+$(function () {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function (start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
+});
 
 
-  findButtonEl.addEventListener('click', searchLocation);
+findButtonEl.addEventListener('click', searchLocation);
